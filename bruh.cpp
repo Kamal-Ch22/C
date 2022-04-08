26e1e1b2c2846f9ca8610f6c5ace0a96fa68068f
Kamal-Ch22 <83061417+Kamal-Ch22@users.noreply.github.com>
2022-04-08 20:33:52 +0530

    Add files via upload

---
+++ bruh.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ff first
+#define ss second
+#define int long long
+#define pb push_back
+#define inf 1e20
+#define sz(a) ((long long)(a).size())
+#define all(x) x.begin(), x.end()
+#define OKAY ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+const int mod = 1e9 + 7;
+int dig (int a) {return floor(log10(a)) + 1;} // digits
+void dbg(int a) {cerr << "a is" << " " << a << endl;}
+ 
+void ez()
+{ 
+int n,m,x,y;
+cin>>n>>m>>x>>y;
+int ver = x;
+int hor=y;
+bool chk=0,first=0,vch=0;
+while (ver<=n)
+{
+   if(!first)
+   {
+    while(hor>=1)
+    {
+      cout<<ver<<" "<<hor<<endl;
+      hor--;
+    }
+    hor=y+1;
+    while(hor<=m)
+    {
+        cout<<ver<<" "<<hor<<endl;
+        hor++;
+    }
+    hor--;
+        if(!vch)
+        {
+          if(ver==1) {ver=x;vch=1;}
+          else ver--;
+        }
+         if(vch) ver++;
+         first=1;
+    }
+   else
+   {
+    if(!chk)
+    {
+        while(hor>=1)
+       {
+          cout<<ver<<" "<<hor<<endl;
+          hor--;
+       }
+        hor++;
+        
+         if(!vch)
+        {
+          if(ver==1) {ver=x;vch=1;}
+          else ver--;
+        }
+         if(vch) ver++;
+     
+    chk=1;
+    }
+    else
+    {
+        while(hor<=m)
+        {
+           cout<<ver<<" "<<hor<<endl;
+          hor++;
+        }
+         hor--;
+         if(!vch)
+        {
+          if(ver==1) {ver=x;vch=1;}
+          else ver--;
+        }
+         if(vch) ver++;
+         chk=0;
+    }
+      
+    }
+}
+ 
+}
+ 
+ 
+ 
+  
+ 
+int32_t main()
+{
+    OKAY
+#ifndef ONLINE_JUDGE
+    //for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    //for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+#endif
+    int t=1;  
+    //cin>>t;
+    while(t--)
+    {
+        ez();
+    }
+return 0;
+ }
